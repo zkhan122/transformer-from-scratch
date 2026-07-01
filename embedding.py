8daf2c5a13eb034a040da998de28a0bd8931db44
Zayaan Khan <zayaankashif@yahoo.com>
2026-07-01 11:32:32 +0100

    using nn.Embedding as lookup table instead of token_mapping(), changed token ids to start from 0 and not random, moved tensors to GPU

--- embedding.py
+++ embedding.py
@@ -8,10 +8,12 @@ import torch.nn as nn
 """
 In Large Language Models (LLMs), Vocabulary Size is the total pool of distinct tokens 
 (words or subwords) the model knows, 
-while Sequence Length is the number of tokens it processes in a single context
- window. A larger vocabulary shortens text, but increases memory, 
+while Sequence Length is the number of tokens it processes in a single context window. 
+A larger vocabulary shortens text, but increases memory, 
 whereas a larger sequence length requires exponentially more compute
 """
+
+cuda_device = torch.device("cuda:0")
 
 class PositionalEmbeddingLayer(nn.Module):
     def __init__(self, d_model, seq_len, vocab_size):
@@ -20,7 +22,7 @@ class PositionalEmbeddingLayer(nn.Module):
         self.d_model = d_model # the size of each embedding vector
         self.vocab_size = vocab_size
         # input_matrix = torch.matmul(self.d_model, self.seq_len) # word2vec 
-        self.input_embeddings = nn.Embedding(self.vocab_size, self.d_model, _freeze=False)
+        self.input_embeddings = nn.Embedding(self.vocab_size, self.d_model, _freeze=False) # lookup table
         weight_embeddings = self.get_positional_encoding(self.seq_len, self.d_model)
         self.positional_embeddings = nn.Embedding(self.seq_len, 
                                                 self.d_model, 
@@ -31,9 +33,10 @@ class PositionalEmbeddingLayer(nn.Module):
         seq_buffer = re.findall(r"[\w']+", seq)
         token_id_buffer = {}
         for i in range(len(seq_buffer)): 
+
             token = seq_buffer[i]
             if token not in token_id_buffer:
-                token_id_buffer[token] = random.randint(1, 10000000000)
+                token_id_buffer[token] = i
         return token_id_buffer
     
     def get_positional_encoding(self, seq_len, d_model, n=10000): 
@@ -44,3 +47,7 @@ class PositionalEmbeddingLayer(nn.Module):
                 positional_enc[position][(2*i)+1] = torch.cos(torch.tensor(position/(math.pow(n, (2*i+1)/d_model))))
         return positional_enc
     
+    def forward(self, input: torch.Tensor):
+        inp_e = self.input_embeddings(input).to(input.device)
+        pos_e = self.positional_embeddings(torch.arange(0, self.seq_len)).to(input.device)
+        return inp_e * math.sqrt(self.d_model) + pos_e
